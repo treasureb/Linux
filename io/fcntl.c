8b58a05f2409d26acef251f818b2098eaa877781
maple <929856540@qq.com>
2018-03-31 19:31:52 -0700

    将文件描述符设置为非阻塞

---
+++ io/fcntl.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int SetNoBlock(int fd)
+{
+    int flag = fcntl(fd,F_GETFL);
+    if(flag < 0){
+        perror("fcntl");
+        return -1;
+    }
+
+    return fcntl(fd,F_SETFL,flag | O_NONBLOCK);
+}
+
+int main()
+{
+    SetNoBlock(0);
+    while(1){
+        char buf[1024] = {0};
+        printf(">");
+        fflush(stdout);
+        ssize_t read_size = read(0,buf,sizeof(buf) -1);
+        if(read_size < 0){
+            perror("read");
+            sleep(1);
+        } else if(read_size == 0){
+            printf("read done!\n");
+        }
+        buf[read_size] = '\0';
+        printf("-> buf:%s \n",buf);
+    }
+    return 0;
+}
