f2f2e1e204582acd91aee0aa6ea44990100df866
treasureb <929856540@qq.com>
2018-08-03 20:56:40 +0800

    加载静态链接库

---
+++ dynamiclibrary/static2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void test2(){
+    printf("second static lib");
+}
+
---
+++ dynamiclibrary/test_static.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(int argc,char** argv){
+    test1();
+    test2();
+    return 1;
+}
